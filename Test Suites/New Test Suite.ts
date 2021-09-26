c403771d715a125612c77266ccb7e2b676dbddc8
darlinf <zkiduxi@gmail.com>
2021-09-26 13:30:03 -0400

    first commit

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>acc70aa0-7a08-4ce8-b1d4-8e4536d64d27</testSuiteGuid>
+   <testCaseLink>
+      <guid>f0a307a5-dba0-498b-99f1-bb270ff0dfc6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>e7292527-7192-42e4-b5c7-7ac53e67b51d</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>4cc10d4d-989b-427b-a73a-beb92cb2af7e</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
